409e54aea279cb1a1edf3f86183b673f4a2efaf2
Guillaume Dorce <polynux.dev@gmail.com>
2025-03-29 22:17:59 +0100

    fix upload multiples

--- app/Filament/Resources/PhotoResource/Pages/UploadPhotos.php
+++ app/Filament/Resources/PhotoResource/Pages/UploadPhotos.php
@@ -8,6 +8,7 @@ use App\Models\PhotoGallery;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\Page;
 use Illuminate\Support\Facades\Storage;
 
@@ -17,6 +18,7 @@ class UploadPhotos extends Page
     protected static string $view = 'filament.resources.photo-resource.pages.upload-photos';
 
     public ?array $data = [];
+    public $defaultAlt = '';
     public PhotoGallery $photoGallery;
 
     public function mount(PhotoGallery $record): void
@@ -29,14 +31,14 @@ class UploadPhotos extends Page
     {
         return $form
             ->schema([
-                FileUpload::make('photos')
+                FileUpload::make('data.photos')
                     ->label('Upload Photos')
                     ->multiple()
                     ->image()
                     ->maxFiles(50)
                     ->directory("photos/{$this->photoGallery->id}")
                     ->required(),
-                TextInput::make('default_alt')
+                TextInput::make('data.default_alt')
                     ->label('Default Alt Text')
                     ->helperText('This will be applied to all uploaded photos. You can change individual alt texts later.')
                     ->maxLength(255),
@@ -48,23 +50,27 @@ class UploadPhotos extends Page
         $data = $this->form->getState();
 
         $photos = [];
-        foreach ($data['photos'] as $path) {
+        foreach ($data['data']['photos'] as $path) {
             $photo = Photo::create([
-                'photo_gallery_id' => $this->record->id,
+                'photo_gallery_id' => $this->photoGallery->id,
                 'path' => $path,
-                'alt' => $data['default_alt'] ?? null,
+                'alt' => $data['data']['default_alt'] ?? $this->defaultAlt,
             ]);
 
             $photos[] = $photo;
         }
 
         // If this is the first upload and no cover photo is set, use the first uploaded photo
-        if (!$this->record->cover_photo_id && count($photos) > 0) {
-            $this->record->update(['cover_photo_id' => $photos[0]->id]);
+        if (!$this->photoGallery->cover_photo_id && count($photos) > 0) {
+            $this->photoGallery->update(['cover_photo_id' => $photos[0]->id]);
         }
 
         $this->reset('data');
 
-        $this->notify('success', 'Photos uploaded successfully');
+        Notification::make()
+            ->title('Photos Uploaded')
+            ->success()
+            ->body('Photos uploaded successfully')
+            ->send();
     }
 }
