e731991ce8b129112c76601aa747a243e757d749
Guillaume Dorce <polynux.dev@gmail.com>
2025-07-18 11:18:17 +0200

    use Fiber to generate thumbnails; fix thumbnails path check

--- app/Console/Commands/GenerateThumbnails.php
+++ app/Console/Commands/GenerateThumbnails.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Photo;
+use Fiber;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class GenerateThumbnails extends Command
 {
@@ -24,17 +26,42 @@ class GenerateThumbnails extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
-        Photo::chunk(100, function ($photos) {
-            foreach ($photos as $photo) {
-                $thunbnailPath = 'thumbnails/' . $photo->path;
-                if (!file_exists($thunbnailPath)) {
+        $fibers = [];
+        $photos = Photo::all();
+
+        $this->info('Generating thumbnails for all photos...');
+
+        $galleryId = 0;
+        foreach ($photos as $photo) {
+            $thumbnailPath = Storage::disk('private')->path('thumbnails/' . $photo->path);
+            if (!file_exists($thumbnailPath)) {
+                if ($photo->photo_gallery_id !== $galleryId) {
+                    $galleryId = $photo->photo_gallery_id;
+                    $this->info("Processing gallery ID: {$galleryId}");
+                }
+                $fibers[] = new Fiber(function () use ($photo) {
                     $photo->generateThumbnail();
                     $photo->save();
+                });
+            }
+        }
+
+        foreach ($fibers as $fiber) {
+            $fiber->start();
+        }
+
+        while ($fibers) {
+            foreach ($fibers as $index => $fiber) {
+                if ($fiber->isTerminated()) {
+                    unset($fibers[$index]);
+                } elseif (!$fiber->isStarted() || $fiber->isSuspended()) {
+                    $fiber->resume();
                 }
             }
-        });
+        }
+
         $this->info('Thumbnails generated successfully!');
     }
 }
